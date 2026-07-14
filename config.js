892f4335beb424bb76a287d2748b3aae27cb4893
Nick van den Heiligenberg | Féju ICT Groep <n.vandenheiligenberg@feju.nl>
2026-07-14 13:47:13 +0200

    Pass VITE_FUNCTION_URL to build

---
+++ config.js
@@ -0,0 +1,2 @@
+export const FUNCTION_URL =
+    "https://ai-autotask-function-cba6h0d9cqdcf3ck.westeurope-01.azurewebsites.net/api/ask?code=gOEicfnThbhCO8PkRmS6sTPWwiBklT9QO-wjh5lSTEyGAzFuFf2_8g==";
